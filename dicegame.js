820bc20d9fe0f0f5d1b00af3cca27aabd613f5bf
tgood111 <tgood111@live.com>
2017-07-21 11:13:01 -0500

    initial commit

---
+++ dicegame.js
@@ -0,0 +1,96 @@
+"use strict"
+
+function dieFour(){
+	var getDieFour = Math.floor(Math.random()*4 + 1);
+	var score;
+	score = getDieFour
+	for(var i = 0; i < string.length; i++) {
+	currentLetter = string[i];
+	if (score === getDieFour[i+1]){
+		counter++;
+	}
+	else {
+		var toPush = counter.toString() + currentLetter;
+		answer.push(toPush);
+		counter = 1;
+	console.log(getDieFour);
+	}
+	var rolledDye = dieFour();
+
+function dieSix(){
+	var getDieSix = Math.floor(Math.random()*6+1);
+	var score;
+	score = getDieSix;
+	console.log(getDieSix);
+}
+var rolledDye = dieSix();
+
+function dieEight(){
+	var getDieEight = Math.floor(Math.random()*8+1);
+	var score;
+	score = getDieEight;
+	console.log(getDieEight);
+}
+var rolledDye = dieEight();
+
+function dieTen(){
+	var getDieTen = Math.floor(Math.random()*10*1);
+	var score;
+	score = getDieTen
+	console.log(getDieTen);
+}
+var rolledDye = dieTen();
+
+function dieTwelve(){
+	var getDieTwelve
+	var score;
+	score = getDieTwelve
+	console.log(getDieTwelve);
+}
+var rolledDye = dieTwelve
+
+function dieTwenty(){
+		var getDieTwenty
+		var score;
+		score = getDieTwenty
+		console.log(getDieTwenty)
+}
+var rolledDye = dieTwenty
+
+
+	//console.log(result}
+//}
+//var numbers = getUserInput();
+//var total = calculateTheSum(numbers);
+//displayRuslt(total);
+
+
+
+
+// pick a random number between 1 and 6 for our roll of the die
+//var die = Math.floor(Math.random()*6 + 1);
+//var score;
+
+// set score here equal to die
+//score = die
+
+//console.log("You rolled a "+die+" for a 
+
+//var die1 = Math.floor(Math.random()*6 + 1);
+//var die2 = Math.floor(Math.random()*6 + 1);
+//var score;
+
+// This time if either die roll is 1 then score should be 0 
+//if()
+
+
+//else {
+  // here we need to check if there are doubles.  If so, score should be
+  // double the sum of the two dice
+  //if()
+  
+  //else
+//}
+
+//console.log("You rolled a "+die1+" and a "+die2+" for a score of "+score);
+//In this version, all of our old rules apply. //But if you roll doubles (if both die are the same) then your score counts for double die1 + die2.//}
